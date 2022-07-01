1bf6a830ddfd5acc32d8844a93b13dec95f9559f
Rodrigo de Sousa <93033013+Rodri-5@users.noreply.github.com>
2022-07-01 15:02:00 +0100

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,16 @@
+print('Hello')
+a = input('Whats your favourite colour?\n')
+if a == "red":
+        print("That's very hot of you")
+elif a == "orange":
+    print("Damn, marry me")
+elif a == "yellow":
+    print("Who hurt you?")
+elif a == "green":
+    print("That's very mlm of you")
+elif a == "blue":
+    print("You are cool")
+elif a == "purple":
+    print("You are the best <3")
+else:
+    print("Just be normal and pick one of the gay colours...")
